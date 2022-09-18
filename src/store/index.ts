ae9c0062cd418500a2c0efad390431d720ddadd6
magnae2016 <magnae2016@gmail.com>
2022-09-18 21:24:33 +0900

    Resolve Property 'products' does not exist on type 'State'.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
 import { InjectionKey } from 'vue'
 import { createStore, Store, useStore as baseUseStore } from 'vuex'
-import { products } from './modules/products'
+import { products, State as ProductsState } from './modules/products'
 
 // define typings for the store state
 export interface State {
   count: number,
+  products: ProductsState
 }
 
 // define injection key
@@ -13,7 +14,7 @@ export const key: InjectionKey<Store<State>> = Symbol()
 export const store = createStore<State>({
   state: {
     count: 0
-  },
+  } as State,
   getters: {
   },
   mutations: {
