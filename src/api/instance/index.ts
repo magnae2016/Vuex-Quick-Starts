eaa9f6af5abffe61728458a379a021503faa4676
magnae2016 <magnae2016@gmail.com>
2022-09-18 23:32:07 +0900

    Add axios instance

---
+++ src/api/instance/index.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosInstance } from "axios";
+
+const APP_BASE_URI = "https://dummyjson.com/";
+
+function create(url: string, options = {}): AxiosInstance {
+  const instance = axios.create(Object.assign({ baseURL: url }, options));
+  return instance;
+}
+
+export const dummyjson = create(APP_BASE_URI, {
+  timeout: 1000
+});
